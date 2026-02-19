362c49ddf44e39f8bd7e08713a8cd6fd449e3207
ShivaniChouhan012 <shivani.chouhan@ksolves.com>
2026-02-19 19:02:55 +0530

    First demo file commited

---
+++ School_Management_System/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "School Management System",
+    'version': "1.0",
+    'author': "Shivani",
+    'insatallable' : True,
+    'Application': True,
+    'category': 'School Management'
+}
